21d3f1f2a9f826f403ae034380a814459a480d18
ReubenHawley <reubenhawley@gmail.com>
2020-07-23 01:39:33 +0200

    seperate home and about html code and created a bse template add css styling link the html files to the views.py

--- car_forum/forum/views.py
+++ car_forum/forum/views.py
@@ -1,8 +1,31 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+posts = [
+    {
+        "author": "reuben Hawley",
+        "title": "check out my first car",
+        "content": "so i bought a new mercedes benz, check it out",
+        "date_posted": "2020/07/23",
+    },
+    {
+        "author": "Yorique jensen",
+        "title": "me and my st",
+        "content": "my st killing it at the track today",
+        "date_posted": "2020/06/18",
+    }
+]
+
 
 def home(request):
-    return HttpResponse('<h1>Forum Home</h1>')
+    context = {
+        "posts": posts
+    }
+    return render(request, "forum/Home.html", context)
 # Create your views here.
 
+
 def about(request):
-    return HttpResponse('<h1>About us</h1>')
+    context = {
+        "posts": posts
+    }
+    return render(request, "forum/About.html",context)
