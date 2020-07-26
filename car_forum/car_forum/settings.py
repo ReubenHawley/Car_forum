4c0a0d1ebc1438a55a0582dcbf2fb663c8905339
ReubenHawley <reubenhawley@gmail.com>
2020-07-27 01:04:49 +0200

    create a new users app create register.html form perform validation check and redirect if successful registration

--- car_forum/car_forum/settings.py
+++ car_forum/car_forum/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'forum.apps.ForumConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
