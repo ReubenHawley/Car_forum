fe82440c2d783fc4761d3dd6f1293f9efce08919
ReubenHawley <reubenhawley@gmail.com>
2020-07-27 00:08:08 +0200

    create database file for posts create posts class for models.py link posts to the sql db

--- car_forum/forum/views.py
+++ car_forum/forum/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        "author": "reuben Hawley",
-        "title": "check out my first car",
-        "content": "so i bought a new mercedes benz, check it out",
-        "date_posted": "2020/07/23",
-        "likes": 51,
-    },
-    {
-        "author": "Yorique jensen",
-        "title": "me and my st",
-        "content": "my st killing it at the track today",
-        "date_posted": "2020/06/18",
-        "likes": 100,
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        "posts": posts
+        "posts": Post.objects.all()
     }
     return render(request, "forum/Home.html", context)
 # Create your views here.
@@ -28,7 +12,7 @@ def home(request):
 
 def about(request):
     context = {
-        "posts": posts
+        "posts": Post.objects.all()
     }
     return render(request, "forum/About.html",context)
 
